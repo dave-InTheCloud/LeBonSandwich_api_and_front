b416df1794fc3767dfc5ab49253fb54c0d1edf61
lambiase1u <davidlambiase@gmail.com>
2016-12-25 22:58:41 +0100

    First push intiliase  archetype (airhacks) + persistence unit + hello world

---
+++ src/main/java/boundary/SandwichRepresentation.java
@@ -0,0 +1,25 @@
+package boundary;
+
+import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+@Path("/sandwich")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Stateless
+public class SandwichRepresentation {
+
+	 @GET
+	 @Path("/")
+	 public	String sayHello(){
+			return "hello wolrd";
+		 
+	 }
+}
---
+++ src/main/java/entity/Sandwich.java
@@ -0,0 +1,50 @@
+package entity;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@XmlRootElement
+public class Sandwich {
+		
+	@Id
+	private String id;
+	private String size;
+	private String typeBread;
+	private List<String>ingredients;
+	
+	
+	public Sandwich(){
+		
+	}
+	
+	public Sandwich(String size, String typeBread, List<String> ingredients) {
+		super();
+		this.size = size;
+		this.typeBread = typeBread;
+		this.ingredients = ingredients;
+	}
+	public String getSize() {
+		return size;
+	}
+	public void setSize(String size) {
+		this.size = size;
+	}
+	public String getTypeBread() {
+		return typeBread;
+	}
+	public void setTypeBread(String typeBread) {
+		this.typeBread = typeBread;
+	}
+	public List<String> getIngredients() {
+		return ingredients;
+	}
+	public void setIngredients(List<String> ingredients) {
+		this.ingredients = ingredients;
+	}
+	
+	
+}
