625a5234461868c3528fa3db61861b94ae8c1141
lambiase1u <davidlambiase@gmail.com>
2016-12-26 17:06:26 +0100

    Add ingredient entity  +  change in persistance unit to keep data after redeployement

--- src/main/java/entity/Ingredient.java
+++ src/main/java/entity/Ingredient.java
@@ -1,10 +1,24 @@
 package entity;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+
+@Entity
+@XmlRootElement
 public class Ingredient {
 
 	private String name;
+	@ManyToOne
+	@JsonBackReference
 	private String category;
 	
+	public Ingredient(){
+		
+	}
 	
 	public Ingredient(String name, String category) {
 		this.name = name;
