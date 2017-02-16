ef96668515f64df4c6d5204e49d07f42332acfb3
demougin2u <mini-d@hotmail.fr>
2017-02-16 19:56:13 +0100

    correction d'un bug lors de l'ajout d'une catégorie, l'attribut limiteNbIngredient se met correctement

--- src/main/java/entity/Category.java
+++ src/main/java/entity/Category.java
@@ -8,7 +8,6 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
@@ -22,62 +21,62 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
     @NamedQuery(name = "Category.findAll", query = "SELECT c FROM Category c")
 })
 public class Category implements Serializable {
-	private static final long serialVersionUID = 1L;
-
-	@Id 
-	private String id;
-	private String name;
-
-	private int limiteNbIngredient;
-	
-	@OneToMany(cascade = CascadeType.ALL, mappedBy = "category", fetch = FetchType.EAGER)
-	@JsonManagedReference
-	private List<Ingredient>ingredients;
-	
-	public Category(){
-		
-	}
-	
-	public Category(String name, List<Ingredient> ingredients, int limiteNbIngredient) {
-		this.name = name;
-		this.ingredients = new ArrayList<Ingredient>();
-		this.limiteNbIngredient = limiteNbIngredient;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-
-	public List<Ingredient> getIngredients() {
-		return ingredients;
-	}
-
-
-	public void setIngredients(List<Ingredient> ingredients) {
-		this.ingredients = ingredients;
-	}
-
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-
-	public String getId() {
-		return this.id;
-	}
-
-	public int getLimiteNbIngredient() {
-		return limiteNbIngredient;
-	}
-
-	public void setLimiteNbIngredient(int limiteNbIngredient) {
-		this.limiteNbIngredient = limiteNbIngredient;
-	}
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    private String id;
+    private String name;
+    
+    private int limiteNbIngredient;
+    
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category", fetch = FetchType.EAGER)
+    @JsonManagedReference
+    private List<Ingredient>ingredients;
+    
+    public Category(){
+        
+    }
+    
+    public Category(String name, List<Ingredient> ingredients, int limiteNbIngredient) {
+        this.name = name;
+        this.ingredients = new ArrayList<Ingredient>();
+        this.limiteNbIngredient = limiteNbIngredient;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+    
+    
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+    
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    
+    public String getId() {
+        return this.id;
+    }
+    
+    public int getLimiteNbIngredient() {
+        return this.limiteNbIngredient;
+    }
+    
+    public void setLimiteNbIngredient(int limiteNbIngredient) {
+        this.limiteNbIngredient = limiteNbIngredient;
+    }
 }
